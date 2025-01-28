65d5e575ae4c37e757a6affe2aedfb30eb43431a
Sown281 <lesondinh02@gmail.com>
2025-01-28 10:55:08 +0700

    Update iloveu.lua

--- iloveu.lua
+++ iloveu.lua
@@ -1,19 +1,52 @@
+-- New draggable Orion Lib script for hub creations!
 local OrionLib = loadstring(game:HttpGet(('https://pastebin.com/raw/WRUyYTdY')))()
 
 
 local Window = OrionLib:MakeWindow({
-    Name = "Good Scripts",
+    Name = "Nerd hub",
     HidePremium = false,
     SaveConfig = false,
     ConfigFolder = "Name" -- Put the name of your hub or script here!
 })
+
+print("Successfully executed.") -- Just a debug
+
 local Tab = Window:MakeTab({
-    Name = "BloxKid",
+    Name = "Bloxkid",
     Icon = "apple",
     PremiumOnly = false
 })
+
+local Section = Tab:AddSection({
+    Name = "scripts"
+})
+
+local playerName = game.Players.LocalPlayer.Name
+
+Tab:AddButton({
+    Name = "Welcome " .. playerName .. "! This is the best hub!",
+    Callback = function()
+    end
+})
+
 Tab:AddButton({
     Name = "Load Redz",
-    Callback = loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
-    end
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+  end    
+})
+
+Tab:AddButton({
+    Name = "Load W-azure",
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+  end    
+})
+
+
+Tab:AddButton({
+    Name = "Load AnDepZai",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/Nameless-Admin/main/Source.lua"))()
+  end    
 })
