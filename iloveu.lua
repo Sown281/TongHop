11d7bc7c6cd789d1059f4cd6e57a4a8ea35b8c9b
Sown281 <lesondinh02@gmail.com>
2025-01-28 10:44:12 +0700

    Create iloveu.lua

---
+++ iloveu.lua
@@ -0,0 +1,14 @@
+local OrionLib = loadstring(game:HttpGet(('https://pastebin.com/raw/WRUyYTdY')))()
+
+
+local Window = OrionLib:MakeWindow({
+    Name = "Good Scripts",
+    HidePremium = false,
+    SaveConfig = false,
+    ConfigFolder = "Name" -- Put the name of your hub or script here!
+})
+local Tab = Window:MakeTab({
+    Name = "BloxKid",
+    Icon = "rbxassetid://1",
+    PremiumOnly = false
+})
