e9a861a340cfa46cc03c4ce5f40740f9de0caefc
Sown281 <lesondinh02@gmail.com>
2025-02-12 11:01:43 +0700

    Update TongHopV1.4.lua

--- TongHopV1.4.lua
+++ TongHopV1.4.lua
@@ -6,17 +6,31 @@ local Window = OrionLib:MakeWindow({
     SaveConfig = false,           
     ConfigFolder = nil,       
     IntroEnabled = true,
-    IntroText = "Welcome/Xin chào/ " .. game.Players.LocalPlayer.Name,
+    IntroText = "Welcome/Xin chào " .. game.Players.LocalPlayer.Name,
     IntroIcon = "rbxassetid://82778010291487"
 })
 
 local Tab = Window:MakeTab({
-    Name = "BF Keyless",
+    Name = "Blox",
     Icon = "rbxassetid://140587862516598",                 -- must use image asset id
     PremiumOnly = false
 })
 
+Tab:AddButton({
+	Name = "Load Redz",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+            print("loaded redz")
+  	end    
+})
 
+Tab:AddButton({
+	Name = "Load Rubu",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/RubuRoblox/refs/heads/main/RubuBF"))()
+            print("Loaded rubu")
+  	end    
+})
 
 
 -- Never forget to add this when using Orion Library
