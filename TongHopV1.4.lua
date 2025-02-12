ba881688d7e579be037e6c4b94e3b172bd0a3859
Sown281 <lesondinh02@gmail.com>
2025-02-12 10:54:39 +0700

    Update TongHopV1.4.lua

--- TongHopV1.4.lua
+++ TongHopV1.4.lua
@@ -1,1 +1,23 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sown281/SomeUI/refs/heads/main/OrionNew.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sown281/SomeUI/refs/heads/main/OrionNew.lua")))()
+
+local Window = OrionLib:MakeWindow({
+    Name = "TongHop | V1.4",
+    HidePremium = false,
+    SaveConfig = false,           
+    ConfigFolder = nil,       
+    IntroEnabled = true,
+    IntroText = "Welcome/Xin chào/ " .. game.Players.LocalPlayer.Name,
+    IntroIcon = "rbxassetid://82778010291487"
+})
+
+local Tab = Window:MakeTab({
+    Name = "BF Keyless",
+    Icon = "rbxassetid://140587862516598",                 -- must use image asset id
+    PremiumOnly = false
+})
+
+
+
+
+-- Never forget to add this when using Orion Library
+OrionLib:Init()
