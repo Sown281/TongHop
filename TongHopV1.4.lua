29fe9b160025ec3e3a64781f24db269390f4a474
Sown281 <lesondinh02@gmail.com>
2025-02-12 19:44:49 +0700

    Update TongHopV1.4.lua

--- TongHopV1.4.lua
+++ TongHopV1.4.lua
@@ -32,6 +32,26 @@ Tab:AddButton({
   	end    
 })
 
+Tab:AddButton({
+	Name = "Load W-azure",
+	Callback = function()
+      		getgenv().Team = "Pirates"
+                getgenv().AutoLoad = false
+        getgenv().SlowLoadUi  = false
+        getgenv().ForceUseSilentAimDashModifier = false
+        getgenv().ForceUseWalkSpeedModifier = false
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+        print("Loaded W-azure")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "Load AnDepZai",
+	Callback = function()
+      		repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiHubBeta/refs/heads/main/AnDepZaiHubNewUpdated.lua"))()
+  	end    
+})
 
 -- Never forget to add this when using Orion Library
 OrionLib:Init()
